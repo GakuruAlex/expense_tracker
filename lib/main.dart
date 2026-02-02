8e9cd1ba9c79c781bd4295eebf198bf52ad52bc8
GakuruAlex <gakurualexgicimo@gmail.com>
2026-02-02 22:30:39 +0300

    Theme change

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,11 @@ import "package:expense_tracker/widgets/expenses.dart";
 import 'package:google_fonts/google_fonts.dart';
 
 final kSeedColor = ColorScheme.fromSeed(
-  seedColor: Color.fromARGB(124, 247, 161, 2),
+  seedColor: Color.fromARGB(123, 22, 22, 22),
 );
 
 final bottomSheetSeedColor = ColorScheme.fromSeed(
-  seedColor: Color.fromARGB(255, 78, 235, 209),
+  seedColor: Color.fromARGB(255, 29, 28, 28),
 );
 final kDarkSeedColor = ColorScheme.fromSeed(
   seedColor: Color.fromARGB(123, 116, 76, 2),
@@ -30,31 +30,37 @@ class MyApp extends StatelessWidget {
           foregroundColor: kSeedColor.onSecondaryContainer,
         ),
         cardTheme: CardThemeData().copyWith(
-          color: Color.fromARGB(255, 78, 235, 209),
+          color: Color.fromARGB(255, 7, 7, 7),
         ),
         bottomSheetTheme: BottomSheetThemeData().copyWith(
           backgroundColor: Color.fromARGB(0, 0, 0, 1),
           constraints: BoxConstraints(maxHeight: 650),
-          modalBackgroundColor: Color.fromARGB(255, 78, 235, 209),
+          modalBackgroundColor: Color.fromARGB(255, 15, 15, 15),
         ),
 
         textTheme: TextTheme(
+          labelLarge: const TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+            color: Color.fromARGB(255, 28, 3, 250),
+          ),
           displayLarge: const TextStyle(
             fontSize: 72,
             fontWeight: FontWeight.bold,
+            color: Color.fromARGB(255, 97, 241, 1),
           ),
           titleLarge: GoogleFonts.oswald(
             fontSize: 30,
             fontStyle: FontStyle.italic,
-            color: Color.fromARGB(255, 31, 30, 30),
+            color: Color.fromARGB(255, 97, 241, 1),
           ),
           bodyMedium: GoogleFonts.merriweather(
             fontSize: 18,
 
-            color: Color.fromARGB(255, 24, 24, 24),
+            color: Color.fromARGB(255, 97, 241, 1),
           ),
           displaySmall: GoogleFonts.pacifico(
-            color: Color.fromARGB(255, 31, 30, 30),
+            color: Color.fromARGB(255, 97, 241, 1),
             fontSize: 18,
           ),
         ),
