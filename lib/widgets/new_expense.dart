899a41c936a5a95df763cb748987a1674072b223
GakuruAlex <gakurualexgicimo@gmail.com>
2026-02-06 21:35:14 +0300

    Usethe new DropdownMenu

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -1,5 +1,6 @@
 import 'package:expense_tracker/main.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
 import 'package:expense_tracker/models/expense.dart';
 
@@ -140,31 +141,79 @@ class _NewExpenseState extends State<NewExpense> {
 
           Row(
             children: [
-              DropdownButton(
-                value: _selectedCategory,
-                items: Category.values.map((category) {
-                  return DropdownMenuItem(
-                    value: category,
-                    child: Text(
-                      category.name.toUpperCase(),
+              Expanded(
+                child: DropdownMenu<Category>(
+                  width: 240,
+                  menuStyle: MenuStyle(
+                    backgroundColor: WidgetStateProperty.all(darkPrimaryColor),
+                    surfaceTintColor: WidgetStatePropertyAll(
+                      onPrimaryTextColor,
                     ),
-                  );
-                }).toList(),
-                onChanged: (value) {
-                  if (value == null) {
-                    return;
-                  }
-                  setState(() {
-                    _selectedCategory = value;
-                  });
-                },
-                icon: Icon(
-                  Icons.arrow_drop_down,
-                  color: accentColor,
-                  size: 36,
+                  ),
+                  trailingIcon: Icon(
+                    Icons.arrow_drop_down_circle_outlined,
+                    color: onPrimaryTextColor,
+                    size: 32,
+                  ),
+                  textStyle: GoogleFonts.oswald(
+                    fontStyle: FontStyle.italic,
+                    color: onPrimaryTextColor,
+                    fontSize: 18,
+                  ),
+                  label: Text(
+                    "Category",
+                    style: GoogleFonts.oswald(color: onPrimaryTextColor),
+                  ),
+                  hintText: "Select a Category",
+                  onSelected: (value) {
+                    if (value == null) {
+                      return;
+                    }
+                    setState(() {
+                      _selectedCategory = value;
+                    });
+                  },
+                  dropdownMenuEntries: Category.values.map((category) {
+                    return DropdownMenuEntry(
+                      style: ButtonStyle(
+                        foregroundColor: WidgetStatePropertyAll(
+                          onPrimaryTextColor,
+                        ),
+                        textStyle: WidgetStatePropertyAll(
+                          GoogleFonts.oswald(fontSize: 18),
+                        ),
+                        backgroundColor: WidgetStateColor.resolveWith(
+                          (states) {
+                            if (states.contains(WidgetState.hovered)) {
+                              return onPrimaryTextColor;
+                            }
+                            if (states.contains(WidgetState.focused)) {
+                              return accentColor;
+                            }
+                            if (states.contains(WidgetState.selected)) {
+                              return lightPrimaryColor;
+                            }
+                            return Colors.transparent;
+                          },
+                        ),
+                        overlayColor: WidgetStatePropertyAll(
+                          onPrimaryTextColor,
+                        ),
+                      ),
+
+                      value: category,
+                      label: category.name.toUpperCase(),
+                    );
+                  }).toList(),
                 ),
               ),
-              const Spacer(),
+            ],
+          ),
+          SizedBox(
+            height: 40,
+          ),
+          Row(
+            children: [
               ElevatedButton(
                 onPressed: () {
                   Navigator.pop(context);
